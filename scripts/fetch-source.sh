8e63f16640e5a2ac1040012606454035f0a7fd00
Rob Taylor <rob.taylor@chipflow.io>
2025-06-07 16:08:44 +0100

    Keep cloned repo size as small as possible

--- scripts/fetch-source.sh
+++ scripts/fetch-source.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-mkdir -p _source
-if [ ! -d _source/xyce ]; then
-  git clone --depth=1 https://github.com/Xyce/Xyce $ROOT/_source/xyce
-else
-  git -C $ROOT/_source/xyce remote set-url origin  https://github.com/Xyce/Xyce
-  git -C $ROOT/_source/xyce checkout master
-  git -C $ROOT/_source/xyce pull --depth=1
-fi
 
-if [ ! -d _source/trilinos ]; then
-  git clone --depth=1 https://github.com/robtaylor/Trilinos -b win-fix  $ROOT/_source/trilinos
-else
-  git -C $ROOT/_source/trilinos remote set-url origin https://github.com/robtaylor/Trilinos
-  git -C $ROOT/_source/trilinos checkout master
-  git -C $ROOT/_source/trilinos pull --depth=1
-fi
+CloneOrUpdate()
+{
+  repo=$1
+  if [ ! -d $repo ]; then
+    git clone --depth=1 https://github.com/Xyce/Xyce $repo
+  else
+    git -C $repo remote set-url origin  https://github.com/Xyce/Xyce
+    git -C $repo checkout master
+    git -C $repo pull --depth=1
+    git -C $repo prune --expire now
+    git -C $repo repack -a -d
+  fi
+}
+
+mkdir -p _source
+CloneOrUpdate $ROOT/_source/xyce
+CloneOrUpdate $ROOT/_source/trilinos
+
 
