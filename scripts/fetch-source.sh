893cda2e70e9d90f6abc3739f69314c4acabe3c6
Rob Taylor <rob.taylor@chipflow.io>
2025-06-07 15:02:46 +0100

    Fix fetch-source to cope with changing repos

--- scripts/fetch-source.sh
+++ scripts/fetch-source.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 mkdir -p _source
 if [ ! -d _source/xyce ]; then
-  git clone https://github.com/Xyce/Xyce _source/xyce
+  git clone https://github.com/Xyce/Xyce $ROOT/_source/xyce
 else
-  git -C xyce pull
+  git -C $ROOT/_source/xyce remote set-url origin  https://github.com/Xyce/Xyce
+  git -C $ROOT/_source/xyce checkout master
+  git -C $ROOT/_source/xyce pull
 fi
+
 if [ ! -d _source/trilinos ]; then
-  git clone https://github.com/robtaylor/Trilinos -b win-fix  _source/trilinos
+  git clone https://github.com/robtaylor/Trilinos -b win-fix  $ROOT/_source/trilinos
+else
+  git -C $ROOT/_source/trilinos remote set-url origin https://github.com/robtaylor/Trilinos
+  git -C $ROOT/_source/trilinos checkout master
+  git -C $ROOT/_source/trilinos pull
 fi
 
