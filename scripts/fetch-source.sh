e8a45f4b1b154699e2665c67ea748fcde03160db
Rob Taylor <rob.taylor@chipflow.io>
2025-06-07 17:23:35 +0100

    Fix borked source fetch

--- scripts/fetch-source.sh
+++ scripts/fetch-source.sh
@@ -2,20 +2,22 @@
 
 CloneOrUpdate()
 {
-  repo=$1
+  url=$1
+  branch=$2
+  repo=$3
   if [ ! -d $repo ]; then
-    git clone --depth=1 https://github.com/Xyce/Xyce $repo
+    git clone --depth=1 $url -b $branch $repo
   else
-    git -C $repo remote set-url origin  https://github.com/Xyce/Xyce
-    git -C $repo checkout master
-    git -C $repo pull --depth=1
+    git -C $repo remote set-url origin $url
+    git -C $repo fetch --depth=1
+    git -C $repo checkout $branch
     git -C $repo prune --expire now
     git -C $repo repack -a -d
   fi
 }
 
 mkdir -p _source
-CloneOrUpdate $ROOT/_source/xyce
-CloneOrUpdate $ROOT/_source/trilinos
+CloneOrUpdate https://github.com/Xyce/Xyce master $ROOT/_source/xyce
+CloneOrUpdate https://github.com/robtaylor/Trilinos win-fix $ROOT/_source/trilinos
 
 
