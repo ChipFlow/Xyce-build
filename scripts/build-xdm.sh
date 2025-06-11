c9334070b0fcdaebbb33c9e5847f27f59cc91b79
Rob Taylor <rob.taylor@chipflow.io>
2025-06-11 21:15:32 +0200

    Set PYTHON_INCLUDE_DIR and PYTHON_LIBRARY for XDM

--- scripts/build-xdm.sh
+++ scripts/build-xdm.sh
@@ -14,6 +14,8 @@ cmake \
 -DCMAKE_INCLUDE_PATH="$INCLUDE_PATH" \
 -DCMAKE_LIBRARY_PATH="$LIBRARY_PATH" \
 -DCMAKE_INSTALL_PREFIX="$ARCHDIR" \
+-DPYTHON_INCLUDE_DIR=$(python -c "import sysconfig; print(sysconfig.get_path('include'))")  \
+-DPYTHON_LIBRARY=$(python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
 $CONFIGURE_OPTS \
 -S "$ROOT/_source/XDM" \
 -B "$ROOT/_build/XDM" 2>&1 | tee "$ROOT/_build/XDM-configure.log"
