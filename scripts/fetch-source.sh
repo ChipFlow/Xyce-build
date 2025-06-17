6431219f05c02fc9a815ebcc84d055a9f3306f64
Rob Taylor <rob.taylor@chipflow.io>
2025-06-17 14:05:38 +0100

    git reset source branches

--- scripts/fetch-source.sh
+++ scripts/fetch-source.sh
@@ -15,6 +15,7 @@ CloneOrUpdate()
     git -C $repo remote set-url origin $url
     git -C $repo fetch --depth=1
     git -C $repo checkout $branch
+    git -C $repo reset --hard origin/$branch
     git -C $repo prune --expire now
     git -C $repo repack -a -d
   fi
@@ -27,7 +28,6 @@ CloneOrUpdate https://github.com/Xyce/Xyce master $ROOT/_source/Xyce
 CloneOrUpdate https://github.com/robtaylor/Xyce_Regression cmake-370-windows $ROOT/_source/Xyce_Regression
 # See https://github.com/Xyce/XDM/issues/11
 CloneOrUpdate https://github.com/robtaylor/XDM fixsstream $ROOT/_source/XDM
-# CloneOrUpdate https://github.com/tvrusso/XDM fixsstream $ROOT/_source/XDM
 
 # Backport KokkosKernels: patch #2296 to 14.4.0
 CloneOrUpdate https://github.com/robtaylor/Trilinos win-fix $ROOT/_source/trilinos
