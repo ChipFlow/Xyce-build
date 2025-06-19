f951a286a24e26f1a8381d6aa67b91094b5de7e6
Rob Taylor <rob.taylor@chipflow.io>
2025-06-19 16:53:40 +0100

    Pass 64bit lapack and blas on windows

--- build.sh
+++ build.sh
@@ -199,6 +199,7 @@ elif [[ "$OS" == "Windows_MSYS2" || "$OS" == "Cygwin" ]]; then
   ./scripts/windows-install.sh
 
   TRILINOS_CONFIGURE_OPTS="-DBLAS_LIBRARY_NAMES=openblas_64 -DBLAS_INCLUDE_DIRS=/ucrt64/include/openblas64 -DLAPACK_LIBRARY_NAMES=lapack64"
+  LDFLAGS="-L/urtc/lib/ -lblas64 -llapack64"
   SUITESPARSE_INC=/ucrt64/include/suitesparse
   LIBRARY_PATH=/ucrt64/lib/x86_64-linux-gnu
   INCLUDE_PATH=/ucrt64/include
@@ -206,7 +207,7 @@ elif [[ "$OS" == "Windows_MSYS2" || "$OS" == "Cygwin" ]]; then
   BOOST_INCLUDEDIR=/ucrt64/include/boost
   BOOST_LIBRARYDIR=/ucrt64/lib/
   PYTHON=/ucrt64/usr/bin/python3
-  export SUITESPARSE_INC LIBRARY_PATH INCLUDE_PATH BOOST_ROOT BOOST_INCLUDEDIR BOOST_LIBRARYDIR
+  export LDFLAGS SUITESPARSE_INC LIBRARY_PATH INCLUDE_PATH BOOST_ROOT BOOST_INCLUDEDIR BOOST_LIBRARYDIR
 
   CFLAGS="$CFLAGS -fpermissive"
   NCPUS=$NUMBER_OF_PROCESSORS
